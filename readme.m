5d1a125e8b62a9c78f486c54c0f2f4d3fd0a0617
Hamdi Boukamcha <hamdouchhd@hotmail.com>
2014-12-13 13:13:41 +0100

    Write Biometric Recognition Matlab Code
    Writer Recognition System V2 : Simple and Effective Source Code For
    Write Recognition Based Edge-Hinge Distribution
    
    Research in automatic writer identification has mainly focused on the
    statistical approach.

---
+++ readme.m
@@ -0,0 +1,45 @@
+
+% 
+% FUNCTIONS
+%
+% Select image:                                  read the input image
+%
+% Add selected image to database:                the input image is added to database and will be used for training
+%
+% Database Info:                                 show informations about the images present in database. 
+%
+% Writer Recognition:                            writer matching. The selected input image is processed
+%
+% Delete Database:                               remove database from the current directory
+%
+% Info:                                          show informations about this software
+%
+%
+% Source code for Writer Recognition System:     how to obtain the complete source code
+%
+% Exit:                                          quit program
+%
+%
+%  References:
+%   L.J.P. van der Maaten and E.O. Postma. Improving Automatic Writer
+%   Identification. In Proceedings of the 17th BNAIC, pages 260-266. Brussels, Belgium, 2005.
+%
+%
+%   IAM Handwriting Database, available at
+%   http://www.iam.unibe.ch/~fki/iamDB/
+%
+%
+% Link Products : http://matlab-recognition-code.com/writer-biometric-recognition-system-matlab-code/
+%
+%
+%
+% Hamdi Boukamcha
+% Tunisia
+% 4081
+% Sousse
+% email  hamdouchhd@hotmail.com
+% mobile +216 50674269
+% web site http://matlab-recognition-code.com
+%
+%
+%
