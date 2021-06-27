704302959413bae8c0374ce817f57060518def14
Hamdi Boukamcha <contact@matlab-recognition-code.com>
2021-06-27 22:20:25 +0200

    Update sourcecode.m

--- sourcecode.m
+++ sourcecode.m
@@ -6,14 +6,6 @@
 % 
 % http://matlab-recognition-code.com/writer-biometric-recognition-system-matlab-code/
 %
-% For any question please email me hamdouchhd@hotmail.com
-%
-% Hamdi Boukamcha
-% Tunisia
-% 4081
-% Sousse
-% email  hamdouchhd@hotmail.com
-% mobile +216 50674269
 % web site http://matlab-recognition-code.com
 %
 %
